60e2cf1da5a6766bf11f83b5dcb3916a6d2447df
Ahmad Rehan <ahmedrehan98@gmail.com>
2024-05-26 15:03:09 +0300

    Add project files.

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [ApiController]
+    [Route("api/cities")]
+    //[Route("api/[controller]")] // [controller] matches "Cities" from CitiesController
+    public class CitiesController: ControllerBase
+    {
+        //[HttpGet("api/cities")]
+        [HttpGet]
+        public JsonResult GetCities()
+        {
+            return new JsonResult(
+                new List<object>
+                {
+                    new { id=1, Name="New York City"},
+                    new { id=2, Name="Antwerp"}
+                });
+        }
+    }
+}
