2d0e30e7c930bec8f6f462c0297b389472e65040
Ahmad Rehan <ahmedrehan98@gmail.com>
2024-05-31 09:41:13 +0300

    use serilog instead of built in logger

--- CityInfo.API/Program.cs
+++ CityInfo.API/Program.cs
@@ -1,8 +1,17 @@
 using Microsoft.AspNetCore.StaticFiles;
+using Serilog;
+
+// configure serilog
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.File("logs/cityinfo.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
 //builder.Logging.ClearProviders(); // removes all configured logging providers
 //builder.Logging.AddConsole(); // adds console logger (added by default?)
+builder.Host.UseSerilog();
 
 // Add services to the container.
 
@@ -11,6 +20,8 @@ builder.Services.AddControllers(options =>
     options.ReturnHttpNotAcceptable = true;
 }).AddNewtonsoftJson()
 .AddXmlDataContractSerializerFormatters();
+
+builder.Services.AddProblemDetails(); // this is required for the exception handling middleware to work
 
 //builder.Services.AddProblemDetails(options =>
 //{
@@ -29,6 +40,11 @@ builder.Services.AddSingleton<FileExtensionContentTypeProvider>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler(); // exception handling middleware should be before other middleware, so that this middleware handles the exceptions happening in other middleware
+}
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
