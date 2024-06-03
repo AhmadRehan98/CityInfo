d1b5b0048c0ebc14a73e7a795a3ccba5d4672693
Ahmad Rehan <ahmedrehan98@gmail.com>
2024-06-03 09:42:35 +0300

    Added a check on city from token. if token city matches points of interest search of the same city, we authorize the api call, otherwise it's forbidden

--- CityInfo.API/Services/CityInfoRepository.cs
+++ CityInfo.API/Services/CityInfoRepository.cs
@@ -92,6 +92,11 @@ namespace CityInfo.API.Services
             _context.PointOfInterests.Remove(pointOfInterest);
         }
 
+        public async Task<bool> CityNameMatchesCityId(string? cityName, int cityId)
+        {
+            return await _context.Cities.AnyAsync(c => c.Id == cityId && c.Name == cityName);
+        }
+
         public async Task<bool> SaveChangesAsync()
         {
             return (await _context.SaveChangesAsync() >= 0); // return true if zero or more entities have been successfully been saved
